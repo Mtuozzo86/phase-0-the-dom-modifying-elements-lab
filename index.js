39e2a68a8937c1823f820696775395f8932cd7e4
Mtuozzo86 <mtuozzo86@gmail.com>
2021-06-08 18:25:13 -0400

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove(main)
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Mike is the champion"
+document.body.appendChild(newHeader)
